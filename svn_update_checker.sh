9f51e03505acad92d7e274db4b055f1a1362159c
chenhuanjie <934507859@qq.com>
2020-12-19 16:31:42 +0800

    0.0.6 patch for user without an account on svn

--- svn_update_checker.sh
+++ svn_update_checker.sh
@@ -8,22 +8,29 @@ svn_password=`grep "^passwd=" ~/.script/svn_update_checker.lock | cut -d'=' -f2-
 if [ ! -d $svn_path ]; then
     echo "path ${svn_path} doesn't exists"; exit 1;
 fi;
-cd $svn_path;
-local_info=`svn info --username $svn_username --password $svn_password`;
+if [ -n $svn_username ]; then
+    local_info=`svn info "${svn_path}" --username $svn_username --password $svn_password`;
+else
+    local_info=`svn info "${svn_path}"`;
+fi;
 if [ $? -ne 0 ]; then
     echo "error occured when trying to get local svn info at ${svn_path}.";
     echo "please check local svn repository path is right and you have the right access.";
     exit -1;
 fi;
-remote_url=`(echo "${local_info}" | grep -i "^repository root" | cut -d' ' -f3)`;
-remote_info=`svn info "${remote_url}" --username $svn_username --password $svn_password`;
+remote_url=`echo "${local_info}" | grep -i "^repository root" | cut -d' ' -f3-100`;
+if [ -n $svn_username ]; then
+    remote_info=`svn info "${remote_url}" --username $svn_username --password $svn_password`;
+else
+    remote_info=`svn info "${remote_url}"`;
+fi;
 if [ $? -ne 0 ]; then
     echo "error occured when trying to get remote svn info at ${remote_url}.";
     echo "please check remote svn repository URL is right and you have the right access.";
     exit -1;
 fi;
-local_version=`(echo "${local_info}" | grep -i "^revision" | cut -d' ' -f2)`
-remote_version=`(echo "${remote_info}" | grep -i "^revision" | cut -d' ' -f2)`
+local_version=`echo "${local_info}" | grep -i "^revision" | cut -d' ' -f2-100`;
+remote_version=`echo "${remote_info}" | grep -i "^revision" | cut -d' ' -f2-100`;
 if [[ remote_version -eq last_notify ]]; then exit 0; fi;
 if [[ local_version -ne remote_version ]]; then
     /usr/bin/osascript -e "display notification \"your current svn repository is behind remote server by $((remote_version-local_version)) versions\" with title \"svn update checker\"";
--- svn_update_checker_setup.sh
+++ svn_update_checker_setup.sh
@@ -10,22 +10,30 @@ if [ ! -d $svn_path ]; then
     echo "path ${svn_path} doesn't exists"; exit 1;
 fi;
 read -p "svn username(leave empty if not have): " svn_username
-read -p "svn password(leave empty if not have): " svn_password
-cd $svn_path;
-local_info=`svn info --username $svn_username --password $svn_password`;
+if [ -n "$svn_username" ]; then
+    read -p "svn password: " svn_password
+    local_info=`svn info "${svn_path}" --username $svn_username --password $svn_password`;
+else
+    local_info=`svn info "${svn_path}"`;
+fi;
 if [ $? -ne 0 ]; then
     echo "error occured when trying to get local svn info at ${svn_path}.";
     echo "please check local svn repository path is right and you have the right access.";
     exit -1;
 fi;
-remote_url=`(echo "${local_info}" | grep -i "^repository root" | cut -d' ' -f3)`;
-remote_info=`svn info "${remote_url}" --username $svn_username --password $svn_password`;
+remote_url=`(echo "${local_info}" | grep -i "^repository root" | cut -d' ' -f3-100)`;
+if [ -n "$svn_username" ]; then
+    remote_info=`svn info "${remote_url}" --username $svn_username --password $svn_password`;
+else
+    remote_info=`svn info "${remote_url}"`;
+fi;
 if [ $? -ne 0 ]; then
     echo "error occured when trying to get remote svn info at ${remote_url}.";
     echo "please check remote svn repository URL is right and you have the right access.";
     exit -1;
 fi;
-local_version=`(echo "${local_info}" | grep -i "^revision" | cut -d' ' -f2)`
+local_version=`(echo "${local_info}" | grep -i "^revision" | cut -d' ' -f2-100)`
+remote_version=`(echo "${remote_info}" | grep -i "^revision" | cut -d' ' -f2-100)`
 echo "current svn repository status:"
 echo "  local path: ${svn_path}"
 echo "  local version: ${local_version}"
@@ -35,15 +43,17 @@ read -p "Is this ok? y/[n]: " ensure
 if [ "$ensure" != "y" ]; then echo "setup cancelled"; exit 0; fi;
 
 if [ ! -d ~/.script ]; then mkdir ~/.script; fi;
-wget -P ~/.script https://github.com/chenhuanjie/sources/releases/download/0.0.5/svn_update_checker.sh;
+wget -P ~/.script https://github.com/chenhuanjie/sources/releases/download/0.0.6/svn_update_checker.sh;
 echo "successfully downloaded checker script";
 chmod +x ~/.script/svn_update_checker.sh;
 if [ $? -ne 0 ]; then
     echo "error occured downloading, setup cancelled"; exit 1;
 fi;
 echo "path=${svn_path}" >> ~/.script/svn_update_checker.lock;
-echo "user=${svn_username}" >> ~/.script/svn_update_checker.lock;
-echo "passwd=${svn_password}" >> ~/.script/svn_update_checker.lock;
+if [ -z "$svn_username" ]; then
+    echo "user=${svn_username}" >> ~/.script/svn_update_checker.lock;
+    echo "passwd=${svn_password}" >> ~/.script/svn_update_checker.lock;
+fi;
 echo "last=${local_version}" >> ~/.script/svn_update_checker.lock;
 
 echo "$cron_text" | grep "^# svn update checker$" > /dev/null 2>&1;
